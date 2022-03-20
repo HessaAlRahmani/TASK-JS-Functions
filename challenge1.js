eb7f1e3cc6f3af3da62306aafb2aec8621079877
H. alrahmani <h.m.alrahmani@gmail.com>
2022-03-20 18:17:36 +0300

    COMPLETED challange 1&2

--- challenge1.js
+++ challenge1.js
@@ -3,7 +3,9 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
-
+function printName() {
+  console.log("Hessa AlRahmani");
+}
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -11,6 +13,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+
+function printAge(Year) {
+  Age = 2022 - Year;
+  console.log(Age);
+}
 
 /**
  * Task 3:
@@ -23,9 +30,29 @@
  * -- tr: it should print `Merhaba NAME`
  */
 
+function printHello(name, language) {
+  if (language === "en") {
+    console.log(`Hello ${name}`);
+  } else if (language === "es") {
+    console.log(`Hola ${name}`);
+  } else if (language === "fr") {
+    console.log(`Bonjour ${name}`);
+  } else if (language === "tr") {
+    console.log(`Merhaba ${name}`);
+  }
+}
+
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+
+function printMax(num1, num2) {
+  if (num1 < num2) {
+    console.log(num2);
+  } else {
+    console.log(num1);
+  }
+}
